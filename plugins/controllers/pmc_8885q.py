799ed2727e4475931de349444de401bf505dc49b
Jonathan Brown <jbrown@rackspace.com>
2017-02-14 09:49:35 -0600

    moved controller_id up to __init__

--- plugins/controllers/pmc_8885q.py
+++ plugins/controllers/pmc_8885q.py
@@ -7,41 +7,42 @@ from storage_enclosures import Enclosure
 from pprint import pprint
 
 class Pmc8885qController(object):
-    def __init__(self, enclosure, binary='/usr/bin/arcconf'):
+    def __init__(self, controller_id, enclosure, binary='/usr/bin/arcconf'):
+        self.controller_id = controller_id
         self.enclosure = enclosure
         self.binary = binary
         self.admin = AdminOS()
         #self.logger = logger
 
 
-    def blink_device(self, controller, device, pd=False):
+    def blink_device(self, device, pd=False):
        if pd:
            pd_loc = Pmc8885qController.derive_pd(self, device)
            device = '%s %s' % (pd_loc[0], pd_loc[1])
            stdout,stderr = self.admin.run('%s IDENTIFY %s DEVICE %s start' % (self.binary,
-                                                                              controller,
+                                                                              self.controller_id,
                                                                               device))
            print stdout
 
        else:
            device = device.split('u')[1]
            stdout,stderr = self.admin.run('%s IDENTIFY %s LOGICALDRIVE %s start' % (self.binary,
-                                                                                    controller,
+                                                                                    self.controller_id,
                                                                                     device))
            print stdout
 
-    def unblink_device(self, controller, device, pd=False):
+    def unblink_device(self, device, pd=False):
         if pd:
             pd_loc = Pmc8885qController.derive_pd(self, device)
             device = '%s %s' % (pd_loc[0], pd_loc[1])
             stdout,stderr = self.admin.run('%s IDENTIFY %s DEVICE %s stop' % (self.binary,
-                                                                              controller,
+                                                                              self.controller_id,
                                                                               device))
             print stdout
         else:
             device = device.split('u')[1]
             stdout,stderr = self.admin.run('%s IDENTIFY %s LOGICALDRIVE %s stop' % (self.binary,
-                                                                                    controller,
+                                                                                    self.controller_id,
                                                                                     device))
             print stdout
             
@@ -60,10 +61,10 @@ class Pmc8885qController(object):
        pd = j.derive_physical_id('8885Q', device).split(" ")
        return pd
        
-    def get_pd_info(self, controller, device):         
+    def get_pd_info(self, device):         
         '''
         '''
-        pd_data = Pmc8885qController._fetchall_pd_info(self,controller)
+        pd_data = Pmc8885qController._fetchall_pd_info(self)
         pd_loc = Pmc8885qController.derive_pd(self, device)
         for pd_pool in pd_data:
             for dev in pd_pool:
@@ -72,50 +73,48 @@ class Pmc8885qController(object):
                    pprint(pd_pool[dev]) 
 
 
-    def uninit(self, controller, device):
+    def uninit(self, device):
         #The uninitialize command clears Adaptec meta-data and any OS partitions from a drive; 
         #existing data on the drive is destroyed. Drives can uninitialized only if they are in 
         #the Raw or Ready state (that is, not part of any logical drive). A drive in the Raw 
         #state has no Adaptec meta-data but may or may not have an OS partition.
         #e.g arcconf UNINIT 1 0 16
         stdout,stderr = self.admin.run('%s UNINIT %s %s' %  (self.binary,
-                                                             controller))
+                                                             self.controller_id))
 
-    def uninit_all(self, controller):
+    def uninit_all(self):
         #The uninitialize command clears Adaptec meta-data and any OS partitions from a drive; 
         #existing data on the drive is destroyed. Drives can uninitialized only if they are in 
         #the Raw or Ready state (that is, not part of any logical drive). A drive in the Raw 
         #state has no Adaptec meta-data but may or may not have an OS partition.
         #e.g arcconf UNINIT 1 ALL
         stdout,stderr = self.admin.run('%s UNINIT %s ALL' %  (self.binary,
-                                                              controller))
+                                                              self.controller_id))
 
-    def remove_ld(self, controller, device):
+    def remove_ld(self, device):
         #Deletes a logical drive, JBOD, or maxCache logical device. 
         #All data stored on the logical drive or JBOD will be lost
         '''
-        :param controller
         :param device
         :returns a boolean. True on success, False on failure.
         '''
         device = device.split('u')[1]
         stdout,stderr = self.admin.run('%s DELETE %s LOGICALDRIVE %s' % (self.binary,
-                                                                         controller,
+                                                                         self.controller_id,
                                                                          device))
         if stderr:
             return False
         else:
             return True
 
-    def get_ld_info(self, controller, device):
+    def get_ld_info(self, device):
         '''
-        :param controller
         :param device
         :returns a dict with logical disk information
         '''
         device = device.split('u')[1]
         stdout,stderr = self.admin.run('%s getconfig %s LD %s' % (self.binary,
-                                                                  controller,
+                                                                  self.controller_id,
                                                                   device))
         try:
             device_info = stdout.split('\n')[5:-8]
@@ -152,17 +151,16 @@ class Pmc8885qController(object):
 
         return ld_info
 
-    def _fetchall_pd_info(self, controller):
+    def _fetchall_pd_info(self):
         '''
         puts output of an arcconf pd listing into a consumable form.
-        :param controller
         :param device
         :returns a list of dictionaries that contai a dictionary of 
          device attributes
         '''
         pd_list = []
         stdout,stderr = self.admin.run('%s getconfig %s PD' % (self.binary,
-                                                               controller))
+                                                               self.controller_id))
         res = re.split(" +Device\ #", stdout)
         for item in res:
             device_id = item.split('\n')[0]
@@ -184,11 +182,11 @@ class Pmc8885qController(object):
 
 
 if __name__ == '__main__':
-    p = Pmc8885qController('sc847')
-    ld_info = p.get_ld_info('1', 'c1u20')
+    p = Pmc8885qController('1','sc847')
+    ld_info = p.get_ld_info('c1u20')
     pprint(ld_info)
     
-    pd_info = p._fetchall_pd_info('1')
+    pd_info = p._fetchall_pd_info()
     pprint(pd_info)
     #p.remove_ld('1','c1u25') 
     #p.get_pd_info('1', 'c1u44', 'sc847')
@@ -197,4 +195,3 @@ if __name__ == '__main__':
     #p.unblink_device('1', 'c1u44', 'sc847')
     #p.blink_device('1', 'c1u44', 'sc847', pd=True)
     #p.unblink_device('1', 'c1u44', 'sc847', pd=True)
-
